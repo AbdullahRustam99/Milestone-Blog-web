a12bc520ba42ad23ca9a48076adefd758c62a2d4
unknown <abdullahrustam12345@gmail.com>
2025-01-03 18:44:14 +0500

    blog Website

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,6 +5,18 @@ import { urlFor } from "@/sanity/lib/image";
 import CommentsBox from "../components/CommentsBox";
 import Header from "../components/Header";
 
+interface DataType {
+   id?:string,
+   mainHeading: string;
+   description: string;
+   blogdate: number;
+   image: {
+     alt: string;
+     asset: {
+       url: string;
+     };
+   };
+ }
 export default async function Pages({
   params,
 }: {
@@ -27,25 +39,27 @@ export default async function Pages({
   const data = await client.fetch(query);
   console.log(data);
 
+  const blogPost = data.find((post:DataType) => post.id === id)
+
   return (
     <>
       <Header />
       <div className="p-10 bg-red-950 w-full h-full flex gap-5 flex-col justify-center items-center">
         <div className="w-[350px] md:w-[600px] p-5 flex flex-col gap-10 justify-center items-center bg-[#ececec] rounded-lg text-black">
           <Image
-            src={urlFor(data[id].image).url()}
-            alt={data[id].image.alt}
+            src={urlFor(blogPost.image).url()}
+            alt={blogPost.image.alt}
             width={600}
             height={600}
           />
           <div className="md:w-[550px] w-[350px] py-5">
             <h1 className="font-extrabold text-[24px]">
-              {data[id].mainHeading}
+              {blogPost.mainHeading}
             </h1>
             <h3 className="text-[15px] pb-5 text-stone-500 font-semibold">
-              {data[id].blogdate}
+              {blogPost.blogdate}
             </h3>
-            <p className="text-[17px]">{data[id].description}</p>
+            <p className="text-[17px]">{blogPost.description}</p>
           </div>
         </div>
         <CommentsBox params={{ id }} />
